ea489e293bfe76ebd943c33e148931445fcb3c88
Dhara <dhara00719@gmail.com>
2024-07-09 02:07:30 +0400

    update user data use case, Unit test

---
+++ app/src/androidTest/java/com/indie/apps/pannypal/domain/usecase/updateUserDataUsecaseTest.kt
@@ -0,0 +1,82 @@
+package com.indie.apps.pannypal.domain.usecase
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.indie.apps.pannypal.data.dao.MerchantDao
+import com.indie.apps.pannypal.data.dao.MerchantDataDao
+import com.indie.apps.pannypal.data.dao.PaymentDao
+import com.indie.apps.pannypal.data.dao.UserDao
+import com.indie.apps.pannypal.data.db.AppDatabase
+import com.indie.apps.pannypal.data.entity.Merchant
+import com.indie.apps.pannypal.data.entity.MerchantData
+import com.indie.apps.pannypal.data.entity.Payment
+import com.indie.apps.pannypal.data.entity.User
+import com.indie.apps.pannypal.di.IoDispatcher
+import com.indie.apps.pannypal.repository.MerchantDataRepository
+import com.indie.apps.pannypal.repository.MerchantRepository
+import com.indie.apps.pannypal.repository.UserRepository
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class updateUserDataUsecaseTest {
+
+    @get:Rule
+    var hiltAndroidRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var appDatabase: AppDatabase
+
+    @Inject
+    lateinit var userRepository: UserRepository
+
+    @IoDispatcher
+    @Inject
+    lateinit var coroutineDispatcher: CoroutineDispatcher
+
+    private lateinit var userDao: UserDao
+
+    @Before
+    fun setUp() {
+        hiltAndroidRule.inject()
+        userDao = appDatabase.userDao()
+    }
+
+    @After
+    fun tearDown() {
+        appDatabase.close()
+    }
+
+    @Test
+    fun update_user_with_email_Test() = runBlocking {
+        val user = User(id = 1, name = "Test User", currency = "AED")
+
+        userDao.insert(user)
+
+        val userWithEmail = user.copy(email = "test@gmail.com")
+
+        val result = updateUserDataUsecase(
+            userRepository = userRepository,
+            userWithEmail,
+            dispatcher = coroutineDispatcher
+        ).invoke()
+
+        assert(result.toList().size == 2)
+
+        val getUser = userDao.getUser()
+        getUser.run {
+            assert(getUser.email == "test@gmail.com")
+        }
+    }
+
+}
---
+++ app/src/main/java/com/indie/apps/pannypal/domain/usecase/updateUserDataUsecase.kt
@@ -0,0 +1,51 @@
+package com.indie.apps.pannypal.domain.usecase
+
+import android.net.http.HttpException
+import androidx.compose.ui.unit.Constraints
+import com.indie.apps.pannypal.data.dao.MerchantDataDao
+import com.indie.apps.pannypal.data.entity.MerchantData
+import com.indie.apps.pannypal.data.entity.User
+import com.indie.apps.pannypal.di.IoDispatcher
+import com.indie.apps.pannypal.data.module.MerchantDataWithName
+import com.indie.apps.pannypal.repository.MerchantDataRepository
+import com.indie.apps.pannypal.repository.MerchantRepository
+import com.indie.apps.pannypal.repository.UserRepository
+import com.indie.apps.pannypal.util.Constant
+import com.indie.apps.pannypal.util.Resource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
+import javax.inject.Inject
+
+class updateUserDataUsecase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val user: User,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher) {
+
+    suspend operator fun invoke() : Flow<Resource<Int>>{
+        return flow{
+
+            try {
+                emit(Resource.Loading<Int>())
+                val id = userRepository.update(user)
+
+                if(id >0)
+                {
+                    emit(Resource.Success<Int>(id))
+                }else{
+                    emit(Resource.Error<Int>("Merchant data not updated"))
+                }
+
+
+            } catch(e: Throwable) {
+                when(e) {
+                    is IOException -> emit(Resource.Error<Int>("Network Failure"))
+                    else -> emit(Resource.Error<Int>("Conversion Error"))
+                }
+            }
+        }.flowOn(dispatcher)
+    }
+
+}
