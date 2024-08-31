eb15ef0d8081e86507fd8416b3533293ec7a70b3
Dhara <dhara00719@gmail.com>
2024-08-31 14:22:54 +0400

    publish v1_1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.indie.apps.pennypal"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "com.indie.apps.pennypal.CustomTestRunner"
         vectorDrawables {
