da347df36ccec059f1daba48ec0fc1a07095ec36
dhara00719 <dhara00719@gmail.com>
2025-03-19 12:33:33 +0400

    remove unused code

--- app/src/main/java/com/indie/apps/pennypal/data/module/MerchantNameAndDetails.kt
+++ app/src/main/java/com/indie/apps/pennypal/data/module/MerchantNameAndDetails.kt
@@ -1,12 +1,7 @@
 package com.indie.apps.pennypal.data.module
 
-import androidx.room.PrimaryKey
-
 data class MerchantNameAndDetails(
-    @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-
     val name: String,
-
     val details: String? = null
 )
